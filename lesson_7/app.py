d267630b5cf655b78d5a95f01862f5bd01175900
Nail Kalimov <nailkalimov@gmail.com>
2025-07-31 16:47:54 +0400

    refactoring for aiogram 3.7<

--- lesson_7/app.py
+++ lesson_7/app.py
@@ -1,9 +1,10 @@
 import asyncio
+import logging
 import os
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
-
+from aiogram.client.default import DefaultBotProperties
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
@@ -21,7 +22,7 @@ from common.bot_cmds_list import private
 
 # ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 bot.my_admins_list = []
 
 dp = Dispatcher()
@@ -45,13 +46,13 @@ async def on_shutdown(bot):
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
 
     dp.update.middleware(DataBaseSession(session_pool=session_maker))
 
     await bot.delete_webhook(drop_pending_updates=True)
-    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
     await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
